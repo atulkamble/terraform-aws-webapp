8b05ef53a29df6de49e15e34eec18578230dd366
Atul Kamble <atul_kamble@hotmail.com>
2024-12-01 15:12:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0453ec754f44f9a4a" # Update for your region
   instance_type = "t2.micro"
   subnet_id     = "subnet-062a4b29a2e90455e"
-  security_groups = [sg-063a85fbe64a0274b]
+  security_groups = [aws_security_group.web_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
